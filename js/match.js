eb92a1abf97174904b01f1c7f4fe59faf5373dc7
balug87 <balug87@gmail.com>
2026-08-23 18:23:52 +0200

    Barrel-elevation aiming: cursor sets gun pitch only; shots follow the barrel line

--- js/match.js
+++
@@ -1,7 +0,0 @@
-/* Temporary bootstrap: load last known-good match.js from the draw-fix commit. */
-(function () {
-  var s = document.createElement('script');
-  s.src = 'https://cdn.jsdelivr.net/gh/balug87/Duel@1c98a389d7bbb66c851188f5449d34b4596448ef/js/match.js';
-  s.crossOrigin = 'anonymous';
-  document.head.appendChild(s);
-})();
