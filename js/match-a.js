9391c5389e6df90b33d40b8dcd4806751e71fa6c
balug87 <balug87@gmail.com>
2026-08-23 18:27:05 +0200

    Test push of real match-a start

--- js/match-a.js
+++ js/match-a.js
@@ -1,1 +1,3 @@
-PLACEHOLDER
+window.__DUEL_MATCH_SRC = '';
+window.__DUEL_MATCH_SRC += atob('Ly8gLS0tLS0tLS0tLSB0aGUgZHVlbCBpdHNlbGYgKHNpZGUgdmlldykgLS0tLS0tLS0tLQpHQi5EdWVsID0gKGZ1bmN0aW9uICgpIHsKICBjb25zdCBXID0g');
+// test chunk start of aiming match
